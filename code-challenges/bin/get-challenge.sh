bedc1f7a07b6d6043fcf929c1c42f95adc890190
MuckT <mcgtom10@gmail.com>
2021-04-03 10:08:30 -0700

    fix: adjust get-challenge shell script to get file in correct format

--- code-challenges/bin/get-challenge.sh
+++ code-challenges/bin/get-challenge.sh
@@ -2,4 +2,4 @@
 
 echo "Getting Code Challenge $1"
 
-curl https://raw.githubusercontent.com/codefellows/seattle-code-301n21/main/class-$1/challenges/challenges-$1.test.js --output ./code-challenges/challenges-$1.test.js
+curl https://raw.githubusercontent.com/codefellows/seattle-code-301n21/main/class-$1/challenges/challenges-$1.test.js --output "./code-challenges/challenges-$1.test.js"
